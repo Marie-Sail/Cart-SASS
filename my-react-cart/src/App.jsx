78c009bcf5d17aa81647609bd2913b06a3809c87
Marie-Sail <marie.saillard.pro@gmail.com>
2023-10-17 09:03:39 +0200

    card

---
+++ my-react-cart/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.scss'
+
+function App() {
+
+  return (
+
+    <section>
+      <img src="https://cdn.pixabay.com/photo/2017/09/08/19/02/zero-2729769_640.png" alt="01001" />
+      <div>
+        <h1>Hey, You !</h1>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur nesciunt perspiciatis atque,
+          aliquid rerum tenetur inventore ipsum modi fuga veritatis,
+          harum odit voluptatem incidunt autem. Laboriosam earum mollitia eligendi delectus.
+        </p>
+      </div>
+    </section>
+
+  )
+}
+
+export default App
